33b2abfc620a8f1d801006856e4a63d3ff92d2ab
13asoleteir <basolteir@gmail.com>
2024-01-02 14:16:55 +0800

    commit push origin master

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -25,32 +25,10 @@ function Projects() {
             <ProjectCard
               imgPath={chatify}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={bitsOfCode}
-              isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={editor}
-              isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="PC Parts"
+              description="PC Parts is a dynamic computer shop website that I designed and developed using Angular, showcasing my proficiency in front-end development and user interface design. This project was a comprehensive exploration of building a responsive and feature-rich e-commerce platform tailored for tech enthusiasts."
+              ghLink="https://github.com/13asoleteir/angular-pc-shop"
+              demoLink=""
             />
           </Col>
 
